b45f2e38e4ddabdbc02a6cbfb98c873525ae15d3
Dooor <dooor1109@gmail.com>
2020-05-10 21:49:46 +0900

    Add joinRoom and exitRoom test

--- test/server/integration/room/mutation.test.ts
+++ test/server/integration/room/mutation.test.ts
@@ -18,6 +18,34 @@ const CREATE_ROOM = gql`
 	}
 `;
 
+const JOIN_ROOM = gql`
+	mutation JoinRoom($invitationCode: String!) {
+		joinRoom(invitationCode: $invitationCode) {
+			id
+			invitationCode
+			capacity
+			players {
+				id
+				name
+			}
+		}
+	}
+`;
+
+const EXIT_ROOM = gql`
+	mutation ExitRoom($roomId: String!) {
+		exitRoom(roomId: $roomId) {
+			id
+			invitationCode
+			capacity
+			players {
+				id
+				name
+			}
+		}
+	}
+`;
+
 describe('Mutations', () => {
 	describe('create room', () => {
 		it('正常系', async () => {
@@ -35,6 +63,44 @@ describe('Mutations', () => {
 
 			expect(errors).toBeUndefined;
 			expect(data).toEqual({ createRoom: mockData });
+		});
+	});
+
+	describe('join room', () => {
+		it('正常系', async () => {
+			const roomId = uuidv4();
+			const mockData = { id: roomId, invitationCode: '123456', capacity: 2, players: [{ id: 'xxxx', name: 'Tester' }] };
+
+			const { server, roomAPI } = constructTestServer({
+				context: async () => ({ session: mockSessionData() })
+			});
+
+			roomAPI.joinRoom = jest.fn(async () => mockData);
+
+			const { mutate } = createTestClient(server);
+			const { data, errors } = await mutate({ mutation: JOIN_ROOM, variables: { invitationCode: '123456' } });
+
+			expect(errors).toBeUndefined;
+			expect(data).toEqual({ joinRoom: mockData });
+		});
+	});
+
+	describe('exit room', () => {
+		it('正常系', async () => {
+			const roomId = uuidv4();
+			const mockData = { id: roomId, invitationCode: '123456', capacity: 2, players: [] };
+
+			const { server, roomAPI } = constructTestServer({
+				context: async () => ({ session: mockSessionData() })
+			});
+
+			roomAPI.exitRoom = jest.fn(async () => mockData);
+
+			const { mutate } = createTestClient(server);
+			const { data, errors } = await mutate({ mutation: EXIT_ROOM, variables: { roomId } });
+
+			expect(errors).toBeUndefined;
+			expect(data).toEqual({ exitRoom: mockData });
 		});
 	});
 });
